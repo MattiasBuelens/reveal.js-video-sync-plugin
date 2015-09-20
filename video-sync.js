cb690214ce6d045399b702d5701f3fbbaf7872a6
Mattias Buelens <mattias.buelens@gmail.com>
2015-09-20 19:32:35 +0200

    Refactored a bit.

--- video-sync.js
+++ video-sync.js
@@ -51,12 +51,7 @@ var RevealVideoSync = (function () {
             return;
         }
         // Clean up
-        if (trackMetadataListener) {
-            video.removeEventListener('loadedmetadata', trackMetadataListener);
-        }
-        if (trackElement) {
-            video.removeChild(trackElement);
-        }
+        removeTrack();
         // Create new track
         trackElement = document.createElement('track');
         trackElement.kind = 'metadata';
@@ -65,12 +60,42 @@ var RevealVideoSync = (function () {
         trackElement.addEventListener('load', function () {
             track = trackElement.track;
             track.mode = 'hidden';
-            track.addEventListener('cuechange', cueChanged);
+            trackLoaded();
         });
         video.appendChild(trackElement);
     }
 
-    function jumpToSlide(hash) {
+    function removeTrack() {
+        if (trackMetadataListener) {
+            video.removeEventListener('loadedmetadata', trackMetadataListener);
+        }
+        if (track) {
+            trackUnloaded();
+            track = null;
+        }
+        if (trackElement) {
+            video.removeChild(trackElement);
+            trackElement = null;
+        }
+    }
+
+    function normalizeSlide(indices) {
+        indices.h = indices.h || 0;
+        indices.v = indices.v || 0;
+        indices.f = indices.f || 0;
+        return indices;
+    }
+
+    function slidesEqual(left, right) {
+        if (!left || !right) {
+            return !left && !right;
+        }
+        return left.h === right.h
+            && left.v === right.v
+            && left.f === right.f;
+    }
+
+    function parseSlide(hash) {
         // Attempt to parse the hash as either an index or name
         var bits = hash.split('/'),
             h, v, f;
@@ -85,31 +110,55 @@ var RevealVideoSync = (function () {
             }
             if (!element) {
                 // Unknown slide
-                return false;
+                return null;
             }
             // Find the position of the named slide
             var indices = Reveal.getIndices(element);
             h = indices.h;
             v = indices.v;
-            f = parseInt(bits[1], 10) || 0;
+            f = parseInt(bits[1], 10);
         }
         else {
             // Index components
-            h = parseInt(bits[0], 10) || 0;
-            v = parseInt(bits[1], 10) || 0;
-            f = parseInt(bits[2], 10) || 0;
+            h = parseInt(bits[0], 10);
+            v = parseInt(bits[1], 10);
+            f = parseInt(bits[2], 10);
         }
 
-        // Jump to slide
-        Reveal.slide(h, v, f);
-        return true;
+        return normalizeSlide({h: h, v: v, f: f});
     }
 
-    function cueChanged() {
+    function getCueSlide(cue) {
+        return parseSlide(cue.text);
+    }
+
+    function getActiveCueSlide() {
         var cues = trackElement.track.activeCues;
-        if (cues.length) {
-            jumpToSlide(cues[0].text);
+        return cues.length && getCueSlide(cues[0]);
+    }
+
+    function jumpToSlide(slide) {
+        Reveal.slide(slide.h || 0, slide.v || 0, slide.f || 0);
+    }
+
+    var activeCueSlide;
+
+    function cueChanged() {
+        // Update slide of active cue
+        activeCueSlide = getActiveCueSlide();
+        if (!activeCueSlide) {
+            return;
         }
+        // Jump to slide
+        jumpToSlide(activeCueSlide);
+    }
+
+    function trackLoaded() {
+        track.addEventListener('cuechange', cueChanged);
+    }
+
+    function trackUnloaded() {
+        track.removeEventListener('cuechange', cueChanged);
     }
 
     function loadVideo(videoUrl, slidesUrl) {
